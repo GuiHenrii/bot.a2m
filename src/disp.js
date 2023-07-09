cd263867252272cd4ed22f66f7db6be3f7db4537
Guilherme Henrique <80778369+yamashitaa@users.noreply.github.com>
2023-07-09 14:00:15 -0300

    Reajustar

--- src/disp.js
+++ src/disp.js
@@ -1,4 +1,3 @@
-import conn from "./db/conn.js";
 import Cliente from "./models/chat.js";
 import schedule from "node-schedule";
 import moment from "moment";
@@ -7,18 +6,18 @@ import { Op } from "sequelize";
 import atualizaStage from "./functions/stage.js";
 import dialogoCaminho1 from "./dialogo/dialogoCaminho1.js";
 import dialogoCaminho2 from "./dialogo/dialogoCaminho2.js";
-import dialogoduvida1 from "./dialogo/dialogoduvida1.js";
-import dialogoduvida2 from "./dialogo/dialogoduvida2.js";
-import dialogoEstante from "./dialogo/dialogoEstante.js";
-import dialogoMovel from "./dialogo/dialogoMovel.js";
-import dialogoPlanejado from "./dialogo/dialogoPlanejado.js";
-import dialogoQuadros from "./dialogo/dialogoQuadros.js";
-import dialogoAtendente from "./dialogo/dialogoAtendente.js";
 import dialogoInicial from "./dialogo/dialogoInicial.js";
 import dialogoOrcamento from "./dialogo/dialogoOrcamento.js";
 import dialogoSac from "./dialogo/dialogoSac.js";
 import dialogoCredencial from "./dialogo/dialogoCredencial.js";
 import dialogofinal from "./dialogo/dialogofinal.js";
+import dialogoEstante from "./dialogo/dialogoEstante.js";
+import dialogoMovel from "./dialogo/dialogoMovel.js";
+import dialogoAtendente from "./dialogo/dialogoAtendente.js";
+import dialogoPlanejado from "./dialogo/dialogoPlanejado.js";
+import dialogoQuadro from "./dialogo/dialogoQuadros.js";
+import dialogoduvida1 from "./dialogo/dialogoduvida1.js";
+import dialogoduvida2 from "./dialogo/dialogoDuvida2.js";
 
 function start(client) {
   console.log("Cliente Venom iniciado!");
@@ -62,141 +61,156 @@ function start(client) {
             );
           });
 
-        atualizaAtendimento(id);
-
-        async function atualizaAtendimento(id) {
-          const atendido = 1;
-          await Cliente.update({ atendido }, { where: { id: id } });
-        }
+        await atualizaAtendimento(id);
       }
 
       console.log("Todas as mensagens foram enviadas!");
     })();
+  });
 
-    // Bote Receptivo
-    client.onMessage(async (message) => {
-      // Verifica se a mensagem não é de grupo
-      if (!message.isGroupMsg) {
-        const tel = message.from.replace(/@c\.us/g, ""); // recebe o número de telefone do cliente
-        const cliente = await Cliente.findOne({
-          raw: true,
-          where: { telefone: tel },
-        }); // Pesquisa o cliente no banco de dados
+  // BotReceptivo
+  client.onMessage(async (message) => {
+    // Verifica se a mensagem não é de grupo
+    if (!message.isGroupMsg) {
+      const tel = message.from.replace(/@c\.us/g, ""); // recebe o número de telefone do cliente
+      const cliente = await Cliente.findOne({
+        raw: true,
+        where: { telefone: tel },
+      }); // Pesquisa o cliente no banco de dados
 
-        if (!cliente) {
-          const telefoneContato = message.from.replace(/@c\.us/g, "");
-          const dados = {
-            nome: message.notifyName,
-            telefone: telefoneContato,
-            atendido: 1,
-            assunto: "contato Whatsapp",
-            stage: 1,
-          };
-          console.log(dados);
-          const cliente = await Cliente.create(dados);
-          if (message.body && dados.stage === 1) {
-            dialogoInicial(client, message);
-            const estado = 2;
-            const dialogo = "dialogoInicial";
-            atualizaStage(cliente.id, estado, dialogo);
-          }
-        } else {
-          const id = cliente.id;
-          const stage = cliente.stage;
-
-          if (message.body === "0") {
-            const id = cliente.id;
-            const dialogo = await Cliente.findOne({
-              attributes: dialogo,
-              where: {
-                id: id,
-              },
-            });
-            switch (dialogo) {
-              case "dialogoInicial":
-                dialogoInicial(client, message);
-                atualizaStage(cliente.id, 2, "dialogoInicial");
-                break;
-              case "dialogoCaminho1":
-                dialogoCaminho(client, message);
-                atualizaStage(cliente.id, 3, "dialogoCaminho1");
-                break;
-              case "dialogoOrcamento":
-                dialogoOrcamento(client, message);
-                atualizaStage(cliente.id, 4, "dialogoOrcamento");
-                break;
-              case "dialogoCaminho2":
-                dialogoSac(client, message);
-                atualizaStage(cliente.id, 3, "dialogoCaminho2");
-                break;
-              case "dialogoAtendente":
-                dialogoSac(client, message);
-                atualizaStage(cliente.id, 4, "dialogoAtendente");
-                break;
-              case "dialogoduvida1":
-                dialogoSac(client, message);
-                atualizaStage(cliente.id, 20, "dialogoduvida1");
-                break;
-              case "dialogoduvida2":
-                dialogoSac(client, message);
-                atualizaStage(cliente.id, 15, "dialogoduvida2");
-                break;
-              case "dialogoEstante":
-                dialogoSac(client, message);
-                atualizaStage(cliente.id, 7, "dialogoEstante");
-                break;
-              case "dialogoMovel":
-                dialogoSac(client, message);
-                atualizaStage(cliente.id, 5, "dialogoMovel");
-                break;
-              case "dialogoOrcamento":
-                dialogoSac(client, message);
-                atualizaStage(cliente.id, 4, "dialogoOrcamento");
-                break;
-              case "dialogoPlanejado":
-                dialogoSac(client, message);
-                atualizaStage(cliente.id, 7, "dialogoPlanejado");
-                break;
-              case "dialogoQuadros":
-                dialogoQuadros(client, message);
-                atualizaStage(cliente.id, 7, "dialogoQuadros");
-                break;
-              case "dialogoSac":
-                dialogoSac(client, message);
-                atualizaStage(cliente.id, 4, "dialogoSac");
-                break;
-              default:
-                break;
+      if (!cliente) {
+        const telefoneContato = message.from.replace(/@c\.us/g, "");
+        const dados = {
+          nome: message.notifyName,
+          telefone: telefoneContato,
+          atendido: 1,
+          assunto: "contato Whatsapp",
+          stage: 1,
+        };
+        console.log(dados);
+        const cliente = await Cliente.create(dados);
+        if (message.body && dados.stage === 1) {
+          // Chama o diálogo inicial
+          dialogoInicial(client, message);
+          const estado = 2;
+          const dialogo = "dialogoinicial";
+          atualizaStage(cliente.id, estado, dialogo);
+        }
+      } else {
+        //chama dialogo inicial
+        const id = cliente.id;
+        const stage = cliente.stage;
+        switch (stage) {
+          case 1:
+            if (message.body) {
+              // chama o diálogo 1
+              dialogoInicial(client, message);
+              const estado = 2;
+              const dialogo = "dialogoinicial";
+              atualizaStage(cliente.id, estado, dialogo);
             }
-          }
+            break;
+          case 2:
+            if (message.body === "1") {
+              dialogoCaminho1(client, message);
+              const estado = 3;
+              const dialogo = "dialogocaminho1";
+              atualizaStage(cliente.id, estado, dialogo);
+            } else if (message.body === "2") {
+              dialogoCaminho2(client, message);
+              const estado = 3;
+              const dialogo = "dialogocaminho2";
+              atualizaStage(cliente.id, estado, dialogo);
+            }
+            break;
+          case 3:
+            if (message.body === "1") {
+              dialogoOrcamento(client, message);
+              const estado = 4;
+              const dialogo = "dialogoorcamento";
+              atualizaStage(cliente.id, estado, dialogo);
+            } else if (message.body === "2") {
+              dialogoSac(client, message);
+              const estado = 4;
+              const dialogo = "dialogoSac";
+              atualizaStage(cliente.id, estado, dialogo);
+            }
+            break;
+          case 4:
+            if (message.body === "1") {
+              dialogoMovel(client, message);
+              const estado = 5;
+              const dialogo = "dialogomovel";
+              atualizaStage(cliente.id, estado, dialogo);
+            } else if (message.body === "2") {
+              dialogoPlanejado(client, message);
+              const estado = 7;
+              const dialogo = "dialogoPlanejado";
+              atualizaStage(cliente.id, estado, dialogo);
+            } else if (message.body === "3") {
+              dialogoEstante(client, message);
+              const estado = 7;
+              const dialogo = "dialogoEstante";
+              atualizaStage(cliente.id, estado, dialogo);
+            } else if (message.body === "4") {
+              dialogoQuadro(client, message);
+              const estado = 7;
+              const dialogo = "dialogoQuadro";
+              atualizaStage(cliente.id, estado, dialogo);
+            } else if (message.body === "5") {
+              dialogoAtendente(client, message);
+              const estado = 7;
+              const dialogo = "dialogoAtendente";
+              atualizaStage(cliente.id, estado, dialogo);
+            }
+            break;
+          case 5:
+            if (message.body === "1" || message.body === "2") {
+              dialogoAtendente(client, message);
+              const estado = 6;
+              const dialogo = "dialogoAtendente";
+              atualizaStage(cliente.id, estado, dialogo);
+            } else if (message.body === "3") {
+              dialogoduvida2(client, message);
+              const estado = 15;
+              const dialogo = "dialogoduvida2";
+              atualizaStage(cliente.id, estado, dialogo);
+            } else if (message.body === "0") {
+              const cliente = await Cliente.findOne({
+                raw: true,
+                where: { id: id },
+              });
+              const newStage = cliente.stage;
+              const estado = newStage - 1;
+              atualizaStage(id, estado);
+            }
+            break;
+          case 7:
+            if (message.body === "1" || message.body === "2") {
+              dialogoAtendente(client, message);
+              const estado = 8;
+              const dialogo = "dialogoAtendente";
+              atualizaStage(cliente.id, estado, dialogo);
+            } else if (message.body === "0") {
+              dialogoOrcamento(client, message);
+              const estado = 5;
+              const dialogo = "dialogoOrcamento";
+              atualizaStage(cliente.id, estado, dialogo);
+            }
+            break;
+          default:
+            break;
         }
       }
-    });
+    }
   });
-}
-  if (stage === 1 && message.body) {
-  dialogoInicial(client, message);
-  const estado = 2;
-  const dialogo = "dialogoInicial";
-  atualizaStage(cliente.id, estado, dialogo);
-} else if (stage === 2) {
-  const message = message.body;
 }
 
-const venomOptions = {
-  multiDevice: false,
-  session: "Testes",
-  createPathFileToken: true,
-  waitForLogin: true,
-  createPathFileToken: true,
-  disableReadReceipts: true, // Desativa a marcação de mensagens lidas
-};
-create(venomOptions)
+// Inicie o Venom Bot aqui
+create({
+  session: "Tendenci", // Nome da sessão
+})
   .then((client) => start(client))
-  .catch((error) => {
-    console.log(error);
+  .catch((erro) => {
+    console.log(erro);
   });
-conn
-  .sync()
-  .then(() => {})
-  .catch((err) => console.log(err));
